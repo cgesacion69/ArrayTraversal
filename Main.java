8d95e6c1b02b3941d4abd45c3f587050407d23b1
cgesacion <cameron.gesacion@patelhs.org>
2020-10-29 18:46:30 +0000

    Switched the overall assignment to buckets and imputed a for loop

--- Main.java
+++ Main.java
@@ -1,16 +1,18 @@
 class Main {
   public static void main(String[] args) {
-    int[ ] highScores = {99,98,98,88,97,84};
-    String[ ] names = { "Ajeet", "Steve", "Rick", "Becky", "Mohan" , "bobby"};
-   System.out.println(names[0] + " has a score of " + highScores[0]);
-   System.out.println(names[1] + " has a score of " + highScores[1]);
-   System.out.println(names[2] + " has a score of " + highScores[2]);
-   System.out.println(names[3] + " has a score of " + highScores[3]);
-   System.out.println(names[4] + " has a score of " + highScores[4]);
-   System.out.println(names[5] + " has a score of " + highScores[5]);
    
-    }
+   int[] buckets = new int[] {5, 10, 15, 20, 25};
+
+int j = 0;
+for (int i = 0; i < buckets.length; i++) {
+ buckets[i] = j;
+ j += 5;
+ System.out.println(buckets[i]);
 }
+  }
+}
+
+
    
 
 
